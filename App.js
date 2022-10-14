b2e81cb91d8fb7e4d0f9789a7fe43451600a7e88
Hugo <hkfe64@gmail.com>
2022-10-14 14:49:15 -0300

    Splash screen até carregar

--- App.js
+++ App.js
@@ -6,16 +6,32 @@ import {
   Montserrat_400Regular,
   Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
+import SplashScreen from "expo-splash-screen";
+
 import cesta from "./src/mocks/cesta";
 
+// Keep the splash screen visible while we fetch resources
 export default function App() {
   let [fontsLoaded] = useFonts({
     MontserratRegular: Montserrat_400Regular,
     MontserratBold: Montserrat_700Bold,
   });
 
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <View></View>;
+    return null;
   }
   return (
     <View>
